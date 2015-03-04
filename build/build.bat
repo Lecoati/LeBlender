95dc3897b7e15775c6a3c6260216296086c145ec
antoine <antoine@lecoati.com>
2015-03-04 18:37:25 +0100

    Leblender nuget

--- build/build.bat
+++ build/build.bat
@@ -4,9 +4,9 @@ ECHO Removing old nupkg
 del /S *.nupkg
 
 ECHO Packing the NuGet release files
-..\.nuget\NuGet.exe Pack Lecoati.BlenderGrid.nuspec
+..\.nuget\NuGet.exe Pack Lecoati.LeBlender.nuspec
 
 ECHO Publish NuGet
-..\.nuget\NuGet.exe Push Lecoati.BlenderGrid*.nupkg
+..\.nuget\NuGet.exe Push Lecoati.LeBlender*.nupkg
 
 PAUSE
