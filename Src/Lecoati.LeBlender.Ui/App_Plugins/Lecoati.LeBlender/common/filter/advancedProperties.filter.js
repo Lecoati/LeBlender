117f0c38685678ff0d7dc5ed65d40b0a297695f2
antoine <antoine@lecoati.com>
2015-04-11 22:40:00 +0200

    More missing files

---
+++ Src/Lecoati.LeBlender.Ui/App_Plugins/Lecoati.LeBlender/common/filter/advancedProperties.filter.js
@@ -0,0 +1,12 @@
+﻿
+angular.module("umbraco").filter('advancedProperties', function () {
+    return function (items, advanced) {
+        var filtered = [];
+        angular.forEach(items, function (item) {
+            if (item.advanced === advanced) {
+                filtered.push(item);
+            }
+        });
+        return filtered;
+    };
+});
---
+++ Src/Lecoati.LeBlender.Ui/App_Plugins/Lecoati.LeBlender/config/editors.config.js
@@ -0,0 +1,34 @@
+﻿[
+    {
+	    "name": "Rich text editor",
+	    "view": "rte",
+	    "configView": "/App_Plugins/Lecoati.LeBlender/editors/default/default.html"
+    },
+    {
+	    "name": "Image",
+	    "view": "media",
+	    "configView": "/App_Plugins/Lecoati.LeBlender/editors/default/default.html"
+    },
+    {
+	    "name": "Macro",
+	    "view": "macro",
+	    "configView": "/App_Plugins/Lecoati.LeBlender/editors/default/default.html"
+    },
+    {
+	    "name": "Embed",
+	    "view": "embed",
+	    "configView": "/App_Plugins/Lecoati.LeBlender/editors/default/default.html"
+    },
+    {
+	    "name": "Textstring",
+	    "view": "textstring",
+	    "configView": "/App_Plugins/Lecoati.LeBlender/editors/textstring/textstringconfig.html"
+    },
+    {
+        "name": "Blender Editor",
+        "view": "/App_Plugins/Lecoati.LeBlender/editors/leblendereditor/LeBlendereditor.html",
+        "configView": "/App_Plugins/Lecoati.LeBlender/editors/leblendereditor/leblendereditorconfig.html"
+    }
+]
+
+       
