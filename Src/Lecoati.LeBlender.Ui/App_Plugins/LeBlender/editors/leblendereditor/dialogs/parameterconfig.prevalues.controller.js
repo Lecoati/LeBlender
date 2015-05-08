02ba680a4f2e7754c6563c0013a50c631a31d86c
antoine <antoine@lecoati.com>
2015-05-08 08:55:45 +0200

    incompatible issue between v0.8 to v1.0.x

--- Src/Lecoati.LeBlender.Ui/App_Plugins/LeBlender/editors/leblendereditor/dialogs/parameterconfig.prevalues.controller.js
+++ Src/Lecoati.LeBlender.Ui/App_Plugins/LeBlender/editors/leblendereditor/dialogs/parameterconfig.prevalues.controller.js
@@ -8,30 +8,30 @@
         if ($scope.dialogData.parameter && $scope.dialogData.parameter.propretyType) {
 
             switch ($scope.dialogData.parameter.propretyType.name) {
-                case "Textstring": $scope.model.value.dataType = "0cc0eba1-9960-42c9-bf9b-60e150b429ae"; 
+                case "Textstring": $scope.dialogData.parameter.dataType = "0cc0eba1-9960-42c9-bf9b-60e150b429ae";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
                 case "Textarea": $scope.model.value.dataType = "c6bac0dd-4ab9-45b1-8e30-e4b619ee5da3";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
-                case "Rich Text Editor": $scope.model.value.dataType = "ca90c950-0aff-4e72-b976-a30b1ac57dad";
+                case "Rich Text Editor": $scope.dialogData.parameter.dataType = "ca90c950-0aff-4e72-b976-a30b1ac57dad";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
-                case "Boolean": $scope.model.value.dataType = "92897bc6-a5f3-4ffe-ae27-f2e7e33dda49";
+                case "Boolean": $scope.dialogData.parameter.dataType = "92897bc6-a5f3-4ffe-ae27-f2e7e33dda49";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
-                case "Media Picker": $scope.model.value.dataType = "93929b9a-93a2-4e2a-b239-d99334440a59";
+                case "Media Picker": $scope.dialogData.parameter.dataType = "93929b9a-93a2-4e2a-b239-d99334440a59";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
-                case "Multi Media Picker": $scope.model.value.dataType = "7e3962cc-ce20-4ffc-b661-5897a894ba7e";
+                case "Multi Media Picker": $scope.dialogData.parameter.dataType = "7e3962cc-ce20-4ffc-b661-5897a894ba7e";
                     $scope.dialogData.parameter.propretyType = {};
                     break;
-                case "Content Picker": $scope.model.value.dataType = "a6857c73-d6e9-480c-b6e6-f15f6ad11125";
+                case "Content Picker": $scope.dialogData.parameter.dataType = "a6857c73-d6e9-480c-b6e6-f15f6ad11125";
                     $scope.dialogData.parameter.propretyType = {};
-                    break;  
+                    break;
                 case "Multi Content Picker":
-                    $scope.model.value.dataType = "";
-                break;
+                    $scope.dialogData.parameter.dataType = "";
+                    break;
             }
 
         }
